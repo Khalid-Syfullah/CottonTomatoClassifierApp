bb60bbcf5ae3303f0c821262bdc1d31f6c84306b
Khalid Syfullah <Khalidsyfullah@gmail.com>
2022-04-18 04:41:48 +0600

    Plant Classifier Update

--- app/src/main/java/com/example/cottontomatoclassifierapp/MainActivity.java
+++ app/src/main/java/com/example/cottontomatoclassifierapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.cottontomatoclassifierapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        cardView = findViewById(R.id.cardView3);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, PlantDiseaseClassifierActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
